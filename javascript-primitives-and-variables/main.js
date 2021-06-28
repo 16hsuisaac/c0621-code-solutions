8481db5aaa9f6b15ef32c02a3008e3e03f01b48e
Isaac Hsu <16hsuisaac@gmail.com>
2021-06-28 10:25:39 -0700

    completed javascript-primitives-and-variables exercise

---
+++ javascript-primitives-and-variables/main.js
@@ -0,0 +1,12 @@
+var fullName = 'Isaac Hsu';
+var isCool = true;
+var totalPets = 0;
+console.log('Full Name:', fullName, ' Data type:', typeof fullName);
+console.log('Is Cool:', isCool, ' Data type:', typeof isCool);
+console.log('Total Pets:', totalPets, ' Data type:', typeof totalPets);
+
+var unknown = null;
+console.log('Unknown:', unknown, ' Data type:', typeof unknown);
+
+var never;
+console.log('Never:', never, ' Data type:', typeof never);
