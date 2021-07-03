fb25c94b88af02702f387494122a982c64b25b31
Isaac Hsu <16hsuisaac@gmail.com>
2021-07-02 18:38:41 -0700

    completed string-manipulation-2

--- string-manipulation-2/src/last-chars.js
+++ string-manipulation-2/src/last-chars.js
@@ -10,3 +10,9 @@ function lastChars(length, string) {
   }
   return newString;
 }
+
+/* 1) create empty string
+2) if length is greater than string length, set new string equal to original string
+3) else loop through all the letters starting from the string length minus length input until the given length
+4) add each letter to the new string
+5) return the new string */
--- string-manipulation-2/src/swap-chars.js
+++ string-manipulation-2/src/swap-chars.js
@@ -1,14 +1,24 @@
 /* exported swapChars */
 function swapChars(firstIndex, secondIndex, string) {
   var newString = '';
+  var firstIndexValue = string[firstIndex];
+  var secondIndexValue = string[secondIndex];
   for (var i = 0; i < string.length; i++) {
-    if (string[firstIndex]) {
-      newString = newString + string[secondIndex];
-    } if (string[secondIndex]) {
-      newString = newString + string[firstIndex];
+    if (i === firstIndex) {
+      newString = newString + secondIndexValue;
+    } else if (i === secondIndex) {
+      newString = newString + firstIndexValue;
     } else {
       newString = newString + string[i];
     }
   }
   return newString;
 }
+
+/* 1) create empty string to push values into
+2) create variables for values at first and second indices
+3) loop through each letter
+4) if i = first index, replace value with variable containing value of second index
+5) else if i = second index, replace value w variable containing value of first index
+6) else add the letter to the string
+7) return new string */
