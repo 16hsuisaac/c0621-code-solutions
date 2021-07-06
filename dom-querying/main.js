85e697c88c1a0b023c346f206421060dc66f76a0
Isaac Hsu <16hsuisaac@gmail.com>
2021-07-06 10:26:40 -0700

    completed dom-querying exercise

---
+++ dom-querying/main.js
@@ -0,0 +1,9 @@
+console.log('hello, world');
+console.log('h1', document.querySelector('h1'));
+console.dir(document.querySelector('h1'));
+console.log('id explanation', document.querySelector('#explanation'));
+console.dir(document.querySelector('#explanation'));
+console.log('class hint', document.querySelector('.hint'));
+console.dir(document.querySelector('.hint'));
+console.log('type p', document.querySelectorAll('p'));
+console.log('attribute example-link', document.querySelectorAll('.example-link'));
