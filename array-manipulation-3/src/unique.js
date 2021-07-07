8ad052befb75a75f452819a45c560e5b961dcc30
Isaac Hsu <16hsuisaac@gmail.com>
2021-07-06 22:37:45 -0700

    completed array-manipulation-3 exercise

--- array-manipulation-3/src/unique.js
+++ array-manipulation-3/src/unique.js
@@ -1,33 +1,30 @@
 /* exported unique */
 function unique(array) {
   var newArray = [];
-  var count = 0;
+  var uniqueIndex = true;
   newArray.push(array[0]);
-  for (var e = 1; e < array.length; e++) {
-    newArray.push(array[e]);
-    for (var o = 0; o < (newArray.length - 1); o++) {
-      if (newArray[e] === newArray[o]) {
-        newArray.splice((e - count), 1);
-        count += 1;
+  for (var i = 1; i < array.length; i++) {
+    for (var e = 0; e < newArray.length; e++) {
+      if (array[i] === newArray[e]) {
+        uniqueIndex = false;
       }
     }
+    if (uniqueIndex === true) {
+      newArray.push(array[i]);
+    }
+    uniqueIndex = true;
+  }
+  if (array.length === 0) {
+    newArray = [];
   }
   return newArray;
 }
 
-/*   for (var a = (newArray.length - 1); a >= 0; a--) {
-    for (var e = 0; e < newArray.length; e++) {
-      if (newArray[a] === newArray[e]) {
-        newArray.splice(a, 1);
-      }
-    }
-  }
-  return newArray;
-}
- */
 /* 1) declare empty array to push values into
 2) push array[0] into the empty array
 3) loop through array starting from index 1
 4) loop through new array
 5) if array index i does not equal any value in the new array
-6) push array[i] to the new array */
+6) push array[i] to the new array
+7) if original array has a length of 0; empty newArray
+8) return newArray */
