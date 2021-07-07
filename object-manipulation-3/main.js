a3a29fb0f5a87a149123a4bdc4df15a500d7ba0d
Isaac Hsu <16hsuisaac@gmail.com>
2021-07-07 13:11:42 -0700

    next assignment

--- object-manipulation-3/main.js
+++ object-manipulation-3/main.js
@@ -1,1 +1,28 @@
 console.log('Lodash is loaded:', typeof _ !== 'undefined');
+
+/* var player1 = { name: 'name1', hand: {} };
+var player2 = { name: 'name2', hand: {} };
+var player3 = { name: 'name3', hand: {} };
+var player4 = { name: 'name4', hand: {} };
+var playerArray = [player1, player2, player3, player4];
+
+var deck = [];
+for (var i = 1; i < 53; i++) {
+  var card = {};
+  if (i < 14) {
+    card.suit = 'clubs';
+    if (i === 1) {
+      card.rank = 'Ace';
+    } else if (i < 11) {
+      card.rank = i;
+    } else if (i === 11) {
+      card.rank = 'Jack';
+    } else if (i === 12) {
+      card.rank = 'Queen';
+    } else if (i === 13) {
+      card.rank = 'King';
+    }
+  } else if (i < 27)
+  }
+}
+ */
