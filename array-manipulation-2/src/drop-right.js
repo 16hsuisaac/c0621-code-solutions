6518da5b07d403d065c60b3b85b3193fd428e227
Isaac Hsu <16hsuisaac@gmail.com>
2021-07-08 15:47:34 -0700

    corrected conditional format

--- array-manipulation-2/src/drop-right.js
+++ array-manipulation-2/src/drop-right.js
@@ -3,7 +3,8 @@ function dropRight(array, count) {
   var newArray = [];
   for (var i = 0; i < (array.length - count); i++) {
     newArray.push(array[i]);
-  } if (count > array.length) {
+  }
+  if (count > array.length) {
     newArray = [];
   }
   return newArray;
