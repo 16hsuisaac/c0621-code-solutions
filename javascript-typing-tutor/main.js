4ff005e4dc4a16f4eefbbc6565f53a928953433d
Isaac Hsu <16hsuisaac@gmail.com>
2021-07-07 23:31:35 -0700

    completed javascript-typing-tutor exercise

--- javascript-typing-tutor/main.js
+++ javascript-typing-tutor/main.js
@@ -1,14 +1,18 @@
 var span = document.querySelectorAll('span');
-
+var i = 0;
 document.addEventListener('keydown', keyPress);
 
 function keyPress(event) {
-  for (var i = 0; i < span.length; i++) {
-    if (event.key === span[i]) {
-      span[i].className = 'correct';
-    } else {
-      span[i].className = 'incorrect';
-      i = i - i;
-    }
+  span[i].className = 'highlight';
+  if (event.key === span[i].textContent) {
+    span[i].className = 'correct';
+    span[i + 1].className = 'highlight';
+    i++;
+  } else if (span[i].textContent.charCodeAt(0) === 160 && event.key === ' ') {
+    span[i].className = 'correct';
+    span[i + 1].className = 'highlight';
+    i++;
+  } else {
+    span[i].className = 'incorrect highlight';
   }
 }
