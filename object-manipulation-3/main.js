3a00aa4fb54148edf5de0d170e9a1abaf3e2a917
Isaac Hsu <16hsuisaac@gmail.com>
2021-07-09 15:27:23 -0700

    cards distributed, need to check card removal and score

--- object-manipulation-3/main.js
+++ object-manipulation-3/main.js
@@ -1,9 +1,9 @@
 console.log('Lodash is loaded:', typeof _ !== 'undefined');
 
-var player1 = { name: 'name1', hand: {} };
-var player2 = { name: 'name2', hand: {} };
-var player3 = { name: 'name3', hand: {} };
-var player4 = { name: 'name4', hand: {} };
+var player1 = { name: 'name1', hand: [], score: 0 };
+var player2 = { name: 'name2', hand: [], score: 0 };
+var player3 = { name: 'name3', hand: [], score: 0 };
+var player4 = { name: 'name4', hand: [], score: 0 };
 var playerArray = [player1, player2, player3, player4];
 
 var deck = [];
@@ -17,18 +17,26 @@ for (var i = 0; i < suit.length; i++) {
     deck.push(card);
   }
 }
-
+console.log(deck);
 /* shuffle */
 function rng() {
-  var num = Math.floor(Math.random() * 52);
+  var num = _.random(0, deck.length);
   return num;
 }
-rng();
+var x = 0;
 
-/* distribute cards */
+/* distribute cards and remove from deck */
 for (var o = 0; o < playerArray.length; o++) {
   for (var u = 0; u < 2; u++) {
-    playerArray[o].hand = deck[rng()];
+    x = rng();
+    playerArray[o].hand.push(deck[x]);
+    _.remove(deck, deck[x]);
+    playerArray[o].score = playerArray[o].score + deck[x].Value;
+    console.log(deck[x].Value);
+    console.log(playerArray[o].score);
   }
 }
 console.log(playerArray);
+console.log(deck);
+
+/* find the winning player */
