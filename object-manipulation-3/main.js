24b28695ed99c3af54362a1c25e4d48263b71727
Isaac Hsu <16hsuisaac@gmail.com>
2021-07-09 12:26:22 -0700

    made deck and shuffle function

--- object-manipulation-3/main.js
+++ object-manipulation-3/main.js
@@ -1,28 +1,34 @@
 console.log('Lodash is loaded:', typeof _ !== 'undefined');
 
-/* var player1 = { name: 'name1', hand: {} };
+var player1 = { name: 'name1', hand: {} };
 var player2 = { name: 'name2', hand: {} };
 var player3 = { name: 'name3', hand: {} };
 var player4 = { name: 'name4', hand: {} };
 var playerArray = [player1, player2, player3, player4];
 
 var deck = [];
-for (var i = 1; i < 53; i++) {
-  var card = {};
-  if (i < 14) {
-    card.suit = 'clubs';
-    if (i === 1) {
-      card.rank = 'Ace';
-    } else if (i < 11) {
-      card.rank = i;
-    } else if (i === 11) {
-      card.rank = 'Jack';
-    } else if (i === 12) {
-      card.rank = 'Queen';
-    } else if (i === 13) {
-      card.rank = 'King';
-    }
-  } else if (i < 27)
+var suit = ['clubs', 'diamonds', 'hearts', 'spades'];
+var rank = ['Ace', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'Jack', 'Queen', 'King'];
+var value = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10];
+
+for (var i = 0; i < suit.length; i++) {
+  for (var a = 0; a < rank.length; a++) {
+    var card = { Suit: suit[i], Rank: rank[a], Value: value[a] };
+    deck.push(card);
   }
 }
- */
+
+/* shuffle */
+function rng() {
+  var num = Math.floor(Math.random() * 52);
+  return num;
+}
+rng();
+
+/* distribute cards */
+for (var o = 0; o < playerArray.length; o++) {
+  for (var u = 0; u < 2; u++) {
+    playerArray[o].hand = deck[rng()];
+  }
+}
+console.log(playerArray);
