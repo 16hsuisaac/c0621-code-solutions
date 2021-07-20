3964553b83c8a0cd2c14f0ea5f19a5b02272ec67
Isaac Hsu <16hsuisaac@gmail.com>
2021-07-20 16:57:20 -0700

    used a variable for the countdown

--- javascript-timers/interval.js
+++ javascript-timers/interval.js
@@ -1,19 +1,12 @@
-setInterval(countDown, 1.0 * 1000);
-
 var h1 = document.querySelector('h1');
+var i = 4;
+var intervalID = setInterval(countDown, 1.0 * 1000);
 
 function countDown() {
-  if (h1.textContent === '...') {
-    h1.textContent = '4';
-  } else if (h1.textContent === '4') {
-    h1.textContent = '3';
-  } else if (h1.textContent === '3') {
-    h1.textContent = '2';
-  } else if (h1.textContent === '2') {
-    h1.textContent = '1';
-  } else if (h1.textContent === '1') {
+  h1.textContent = i;
+  if (i === 0) {
     h1.textContent = '~Earth Beeeelooowww Us~';
-  } else if (h1.textContent === '~Earth Beeeelooowww Us~') {
-    clearInterval(setInterval(countDown, 1.0 * 1000));
+    clearInterval(intervalID);
   }
+  i = i - 1;
 }
