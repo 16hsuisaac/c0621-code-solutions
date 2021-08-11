ad88f24cea1ccbf038ed768c8f8d1e8652679a8a
Isaac Hsu <16hsuisaac@gmail.com>
2021-08-11 16:48:24 -0700

    fixed last bit of code for level 3

--- node-fs-readfile/cat.js
+++ node-fs-readfile/cat.js
@@ -3,12 +3,10 @@ const array = [];
 
 for (let i = 2; i < process.argv.length; i++) {
   array.push('');
-  try {
-    const filename = process.argv[i];
-    fs.readFile(filename, 'utf8', (err, data) => {
-      if (err) throw err;
-      array.splice(i, 1, data);
-      console.log(array[i]);
-    });
-  } catch (err) {}
+  const filename = process.argv[i];
+  fs.readFile(filename, 'utf8', (err, data) => {
+    if (err) throw err;
+    array.splice(i, 1, data);
+    console.log(array[i]);
+  });
 }
