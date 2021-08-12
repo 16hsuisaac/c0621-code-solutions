c93917b7f1295fb4c633e98073de534710476924
Isaac Hsu <16hsuisaac@gmail.com>
2021-08-12 09:54:42 -0700

    temp break

--- node-fs-readfile/cat.js
+++ node-fs-readfile/cat.js
@@ -3,10 +3,13 @@ const array = [];
 
 for (let i = 2; i < process.argv.length; i++) {
   array.push('');
+}
+
+for (let i = 2; i < process.argv.length; i++) {
   const filename = process.argv[i];
   fs.readFile(filename, 'utf8', (err, data) => {
     if (err) throw err;
-    array.splice(i, 1, data);
-    console.log(array[i]);
+    array.splice(i - 2, 1, data);
+    console.log(array[i - 2]);
   });
 }
