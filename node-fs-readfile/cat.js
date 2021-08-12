b292857c00ba12756358907d3e1bd544a6a48665
Isaac Hsu <16hsuisaac@gmail.com>
2021-08-12 13:28:39 -0700

    completed level 3

--- node-fs-readfile/cat.js
+++ node-fs-readfile/cat.js
@@ -1,5 +1,6 @@
 const fs = require('fs');
 const array = [];
+let a = 2;
 
 for (let i = 2; i < process.argv.length; i++) {
   array.push('');
@@ -10,6 +11,20 @@ for (let i = 2; i < process.argv.length; i++) {
   fs.readFile(filename, 'utf8', (err, data) => {
     if (err) throw err;
     array.splice(i - 2, 1, data);
-    console.log(array[i - 2]);
+    while (true) {
+      if (array[i - 2] !== '') {
+        break;
+      }
+    }
+    pleaseWork(array);
   });
 }
+
+const pleaseWork = array => {
+  if (a === process.argv.length - 1) {
+    for (let i = 0; i < array.length; i++) {
+      console.log(array[i]);
+    }
+  }
+  a++;
+};
