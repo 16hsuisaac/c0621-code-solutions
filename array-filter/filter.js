601a153e445aeb99b7122bcd7c4c6d59a94cc6ce
Isaac Hsu <16hsuisaac@gmail.com>
2021-08-20 11:41:49 -0700

    used truthiness instead of strictly equal to true

--- array-filter/filter.js
+++ array-filter/filter.js
@@ -1,7 +1,7 @@
 function filter(array, predicate) {
   const newArray = [];
   for (let i = 0; i < array.length; i++) {
-    if (predicate(array[i]) === true) {
+    if (predicate(array[i])) {
       newArray.push(array[i]);
     }
   }
