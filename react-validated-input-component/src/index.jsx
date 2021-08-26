3ff52fe6462bc9e2907a576b9ac9a7dd65f29ae9
Isaac Hsu <16hsuisaac@gmail.com>
2021-08-26 16:01:45 -0700

    completed react-validated-input-component without the extras

---
+++ react-validated-input-component/src/index.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import Validatedinput from './validated-input';
+
+ReactDOM.render(
+  <Validatedinput />,
+  document.querySelector('#root')
+);
