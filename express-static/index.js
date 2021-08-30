709845e1d61e8909b3fcff37b0067638484e2655
Isaac Hsu <16hsuisaac@gmail.com>
2021-08-30 10:26:11 -0700

    completed express-static exercise

---
+++ express-static/index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+
+app.listen(3000, () => {
+  console.log('server is listening on port 3000');
+});
+
+const joined = path.join(__dirname, 'public');
+console.log('joined', joined);
+const expressStatic = app.use(express.static('public'));
+console.log('expressStatic', expressStatic);
+app.use(express.json());
